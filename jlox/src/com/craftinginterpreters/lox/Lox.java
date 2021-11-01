447eeea067015d19cbda7403d30cac3cd9778c01
Juan Bono <juanbono94@gmail.com>
2021-11-01 00:43:56 -0300

    finish parsing chapter

--- jlox/src/com/craftinginterpreters/lox/Lox.java
+++ jlox/src/com/craftinginterpreters/lox/Lox.java
@@ -48,7 +48,13 @@ public class Lox {
 	private static void run(String source) {
 		Scanner scanner = new Scanner(source);
 		List<Token> tokens = scanner.scanTokens();
-		System.out.println(tokens);
+		Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+
+        // Stop if there was a syntax error.
+        if (hadError) return;
+
+        System.out.println(new AstPrinter().print(expression));
 	}
 
 	static void error(int line, String message) {
